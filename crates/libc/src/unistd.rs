7f4f7b1bf621b2d68ec0bf21eeda09ff6f60f6f4
V-Fries <vincentfries@proton.me>
2025-02-24 06:06:45 +0100

    build: Updated rust edition to 2024

--- crates/libc/src/unistd.rs
+++ crates/libc/src/unistd.rs
@@ -1,7 +1,7 @@
 use std::ffi::c_int;
 
 #[link(name = "c")]
-extern "C" {
+unsafe extern "C" {
     pub fn fork() -> crate::sys::types::Pid;
 
     pub fn dup2(old_fd: c_int, new_fd: c_int) -> c_int;
